ff403428d334c8b17ee128008da8a36db8cfc710
jethalalghada <nqvadi@gmail.com>
2022-06-13 17:44:11 +0530

    first commit to router

---
+++ src/Screens/AboutUsPage.js
@@ -0,0 +1,17 @@
+import React  from "react";
+import { NavLink } from "react-router-dom";
+import HeaderComponent from "../Components/HeaderComponent";
+
+const AboutUsPage = () =>
+{
+    return (
+        <>
+       <HeaderComponent />
+    <section>
+        About us page is loaded
+    </section>
+    </>
+    );
+}
+
+export default AboutUsPage;
---
+++ src/Screens/HomePage.js
@@ -0,0 +1,11 @@
+import React  from "react";
+import { NavLink } from "react-router-dom";
+import HeaderComponent from "../Components/HeaderComponent";
+const HomePage = () =>
+{
+    return (
+        <HeaderComponent />
+    );
+}
+
+export default HomePage;
